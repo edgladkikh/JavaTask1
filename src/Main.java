fbbd9b5a78b5d82cadf4bc94f27e4458c4ffd345
EkaterinaGladkikh <hedgehog96174@gmail.com>
2025-02-28 15:21:17 +0300

    AddTask1

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int cost = 7900; // стоимость билета
+        int bonus = 20; // стоимость 1 мили
+        int x = cost / bonus; //количество начисленных миль
+        System.out.println (x);
+    }
+}

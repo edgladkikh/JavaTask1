0d393c5d2c745b2da61735fd193c1dc8bf11cf04
EkaterinaGladkikh <hedgehog96174@gmail.com>
2025-03-01 10:23:18 +0300

    correction

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         int cost = 7900; // стоимость билета
         int bonus = 20; // стоимость 1 мили
-        int x = cost / bonus; //количество начисленных миль
-        System.out.println ((x) + " бонусных миль ");
+        int mili = cost / bonus; //количество начисленных миль
+        System.out.println ((mili) + " бонусных миль ");
     }
 }
